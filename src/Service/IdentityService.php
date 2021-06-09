56b6bba89887a27e3c10ea74bd84805b2b516da7
brandinchiu <brandinchiu@gmail.com>
2021-06-09 09:52:34 -0400

    added identity service call to get credentials

---
+++ src/Service/IdentityService.php
@@ -0,0 +1,72 @@
+<?php
+
+
+namespace Spoonity\Service;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class IdentityService
+ * @package Spoonity\Service
+ */
+class IdentityService
+{
+    /**
+     * @param int $identityId
+     * @param string $token
+     * @return array|null
+     * @throws \Exception
+     */
+    public function getCredentials(int $identityId, string $token): ?array
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            sprintf('Authorization: Bearer %s', $token)
+        ]);
+
+        curl_setopt($ch, CURLOPT_URL, sprintf("https://identity.spoonity.com/identities/%d/credentials", $identityId));
+
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+
+        $result = (curl_exec($ch));
+        $errorCode = curl_errno($ch);
+        $info = curl_getinfo($ch);
+
+        curl_close($ch);
+
+        switch($errorCode) {
+            case CURLE_OK:
+                break;
+
+            case CURLE_OPERATION_TIMEOUTED:
+                throw new \Exception('Could not connect to identity service');
+
+            default:
+                throw new \Exception('Error encountered during api request');
+        }
+
+        /**
+         * set token to null on HTTP failure.
+         */
+        switch($info['http_code']){
+            case Response::HTTP_OK:
+            case Response::HTTP_ACCEPTED:
+            case Response::HTTP_NO_CONTENT:
+                break;
+
+            default:
+                $result = null;
+        }
+
+        return ($result != null) ? json_decode($result, true) : null;
+    }
+}
