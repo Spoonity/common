982b52cbdb0848ffa642d460d544670beb042966
brandinchiu <brandinchiu@gmail.com>
2021-06-28 16:29:00 -0400

    added API for getting all identities.

--- src/Service/IdentityService.php
+++ src/Service/IdentityService.php
@@ -16,8 +16,66 @@ class IdentityService
     const CREDENTIAL_TYPE_WAREHOUSE = 'warehouse';
 
     /**
+     * @param string $token
+     * @return array|null
+     * @throws \Exception
+     */
+    public function getIdentities(string $token): ?array
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            sprintf('Authorization: Bearer %s', $token)
+        ]);
+
+        curl_setopt($ch, CURLOPT_URL, 'https://identity.spoonity.com/identities/');
+
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+
+        $result = (curl_exec($ch));
+        $errorCode = curl_errno($ch);
+        $info = curl_getinfo($ch);
+
+        curl_close($ch);
+
+        switch($errorCode) {
+            case CURLE_OK:
+                break;
+
+            case CURLE_OPERATION_TIMEOUTED:
+                throw new \Exception('Could not connect to identity service');
+
+            default:
+                throw new \Exception('Error encountered during api request');
+        }
+
+        /**
+         * set token to null on HTTP failure.
+         */
+        switch($info['http_code']){
+            case Response::HTTP_OK:
+            case Response::HTTP_ACCEPTED:
+            case Response::HTTP_NO_CONTENT:
+                break;
+
+            default:
+                $result = null;
+        }
+
+        return ($result != null) ? json_decode($result, true) : null;
+    }
+
+    /**
      * @param int $identityId
      * @param string $token
+     * @param string $type
      * @return array|null
      * @throws \Exception
      */
